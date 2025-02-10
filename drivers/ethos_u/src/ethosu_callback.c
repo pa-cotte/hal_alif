f417730d50d8b3d73e3469759bf74a4d54c3ec14
Srinivasa Rao Ragolu <srinivasa@alifsemi.com>
2025-02-10 16:53:20 +0530

    sample: ethos: Add ethos cache handling callbacks
    Adding ethos cache handling callbacks implementations
    
    Signed-off-by: Srinivasa Rao Ragolu <srinivasa@alifsemi.com>

--- drivers/ethos_u/src/ethosu_callback.c
+++ drivers/ethos_u/src/ethosu_callback.c
@@ -5,6 +5,7 @@
 
 #include <stdint.h>
 #include <soc_memory_map.h>
+#include <cmsis_core.h>
 
 uint64_t ethosu_address_remap(uint64_t address, int index)
 {
@@ -12,5 +13,23 @@ uint64_t ethosu_address_remap(uint64_t address, int index)
 
 	/* Double cast to avoid build warning about pointer/integer size mismatch */
 	return local_to_global((void *) (uint32_t) address);
+}
+
+void ethosu_flush_dcache(uint32_t *p, size_t bytes)
+{
+       if (p) {
+               SCB_CleanDCache_by_Addr(p, bytes);
+       } else {
+               SCB_CleanDCache();
+       }
+}
+
+void ethosu_invalidate_dcache(uint32_t *p, size_t bytes)
+{
+       if (p) {
+               SCB_InvalidateDCache_by_Addr(p, bytes);
+       } else {
+               SCB_InvalidateDCache();
+       }
 }
 
